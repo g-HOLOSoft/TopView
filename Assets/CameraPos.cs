7e5599036049bb416182c7eb3057d3f9377497bc
g-HOLOSoft <g.holosoft@gmail.com>
2023-12-05 21:18:44 +0900

    주석 추가

--- Assets/CameraPos.cs
+++ Assets/CameraPos.cs
@@ -6,6 +6,7 @@ public class CameraPos : MonoBehaviour {
     public GameObject player;
     Vector3 pos = new Vector3(0f, 15f, -10f);
 
+    //거리를 유지하면서 따라 다니게...
     void Update() {
         transform.position = player.transform.position + pos;
     }
